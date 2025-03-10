0bfb87a341673ada3335e56d8ad01a566bc3e9c0
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-03-10 14:04:04 +0630

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 Posts::Engine.routes.draw do
-    namespace :api, defaults: {format: 'json'} do
-      namespace :v1 do
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
 
-        resources :drafted_statuses, only: [:create, :index, :show, :update, :destroy] do
-          member do
-            post :publish
-          end
+      resources :drafted_statuses, only: [:create, :index, :show, :update, :destroy] do
+        member do
+          post :publish
         end
-
       end
+
     end
+  end
 end
 
