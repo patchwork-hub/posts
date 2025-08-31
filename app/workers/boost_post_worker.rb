fa171462f33045adb0d660615bd78ab92f8350ea
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-08-31 13:52:55 +0630

    Remove logging of start processing message in BoostPostWorker

--- app/workers/boost_post_worker.rb
+++ app/workers/boost_post_worker.rb
@@ -3,7 +3,6 @@ class BoostPostWorker
   sidekiq_options queue: 'default', retry: false, dead: true
 
   def perform(post_url)
-    Rails.logger.info "[BoostPostWorker] Start processing: #{post_url}"
     Posts::BoostPostService.new(post_url).call
   rescue => e
     Rails.logger.error "[BoostPostWorker] Error processing #{post_url}: #{e.class} - #{e.message}"
