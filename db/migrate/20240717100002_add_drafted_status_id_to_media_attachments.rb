0255960bd9204e817d2ddd7b71d5af976f83e913
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-11-17 17:28:31 +0630

    Refactor migration to conditionally add drafted status reference and index

--- db/migrate/20240717100002_add_drafted_status_id_to_media_attachments.rb
+++ db/migrate/20240717100002_add_drafted_status_id_to_media_attachments.rb
@@ -4,8 +4,13 @@ class AddDraftedStatusIdToMediaAttachments < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
 
   def change
-    safety_assured { add_reference :media_attachments, :patchwork_drafted_status, foreign_key: { on_delete: :nullify }, index: false }
-    add_index :media_attachments, :patchwork_drafted_status_id, algorithm: :concurrently
+    unless column_exists?(:media_attachments, :patchwork_drafted_status_id)
+      safety_assured { add_reference :media_attachments, :patchwork_drafted_status, foreign_key: { on_delete: :nullify }, index: false }
+    end
+    
+    unless index_exists?(:media_attachments, :patchwork_drafted_status_id)
+      add_index :media_attachments, :patchwork_drafted_status_id, algorithm: :concurrently
+    end
   end
 end
 
