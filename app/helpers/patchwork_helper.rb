bb0c811e95a2fee645428e0b6e92d199d5114ea9
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2026-02-02 23:41:27 +0630

    Update patchwork_helper.rb

--- app/helpers/patchwork_helper.rb
+++ app/helpers/patchwork_helper.rb
@@ -4,12 +4,16 @@ module PatchworkHelper
   extend ActiveSupport::Concern
 
   def patchwork_server_settings_exist?
+    return false unless patchwork_table_exists?('server_settings')
+
     return false unless Object.const_defined?('Posts::ServerSetting') && defined?(Posts::ServerSetting) && Posts::ServerSetting.respond_to?(:find_by)
     
     true
   end
 
   def patchwork_community_admin_exist?
+    return false unless patchwork_table_exists?('patchwork_communities_admins')
+
     return false unless Object.const_defined?('Posts::CommunityAdmin') && defined?(Posts::CommunityAdmin) && Posts::CommunityAdmin.respond_to?(:find_by)
     
     true
